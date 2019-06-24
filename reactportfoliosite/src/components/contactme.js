012e5dfb83426186a55a0c8e220a50731d59511e
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2019-06-24 15:47:01 -0400

    Projects Page
    projects.js has been created

--- reactportfoliosite/src/components/contactme.js
+++ reactportfoliosite/src/components/contactme.js
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>ContactMe Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Orkun Ozgiray</h2>
+                        <img 
+                            src="https://image.flaticon.com/icons/svg/877/877927.svg" 
+                            alt="avatar" 
+                            style={{height: '250px'}} />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>asdahsd nkj sdsdks rrrklld jddkdl dkddkdkkd
+                        aas dffggf assas fgg ddkld skdsl sdknsd uueueuekll</p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div classNmae="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (123) 456-7890
+                                        Bryan Cranston
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (102) 738-6745
+                                        Amanda Peiling
+                                    </ListItemContent>
+                                </ListItem>
+                                
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (102) 738-6745
+                                        Amanda Peiling
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
--- reactportfoliosite/src/components/projects.js
+++ reactportfoliosite/src/components/projects.js
@@ -1,9 +1,116 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toogleCategories() {
+        if(this.state.activeTab === 0){
+            return(
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{minWidth: "450", margin: "auto"}}>
+                        <CardTitle style={{color: 'white', height: '176px', background:
+                        'url(https://cdn-images-1.medium.com/max/1200/1*jDIj2SKAE-Bp32owLoHDjw.png) center / cover'}} >React Project #1</CardTitle>
+                        <CardText>
+                            Lorem IpmsSLHDFKAJKD HDJa shdsjbak hsvahs bjsdsdn aksaksj asgasavs kgfeunvldhs vhdvdsnsk
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github1</Button>
+                            <Button colored>Github2</Button>
+                            <Button colored>Github3</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{minWidth: "450", margin: "auto"}}>
+                        <CardTitle style={{color: 'white', height: '176px', background:
+                        'url(https://cdn-images-1.medium.com/max/1200/1*jDIj2SKAE-Bp32owLoHDjw.png) center / cover'}} >React Project #1</CardTitle>
+                        <CardText>
+                            Lorem IpmsSLHDFKAJKD HDJa shdsjbak hsvahs bjsdsdn aksaksj asgasavs kgfeunvldhs vhdvdsnsk
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github1</Button>
+                            <Button colored>Github2</Button>
+                            <Button colored>Github3</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{minWidth: "450", margin: "auto"}}>
+                        <CardTitle style={{color: 'white', height: '176px', background:
+                        'url(https://cdn-images-1.medium.com/max/1200/1*jDIj2SKAE-Bp32owLoHDjw.png) center / cover'}} >React Project #1</CardTitle>
+                        <CardText>
+                            Lorem IpmsSLHDFKAJKD HDJa shdsjbak hsvahs bjsdsdn aksaksj asgasavs kgfeunvldhs vhdvdsnsk
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github1</Button>
+                            <Button colored>Github2</Button>
+                            <Button colored>Github3</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>    
+
+
+            )
+        } else if(this.state.activeTab === 1) {
+            return (
+                <div><h1>This is Java</h1></div>
+            )
+        } else if(this.state.activeTab === 2) {
+            return (
+                <div><h1>This is C#</h1></div>
+            )
+        } else if(this.state.activeTab === 3) {
+            return (
+                <div><h1>This is Swift</h1></div>
+            )
+        } else if(this.state.activeTab === 4) {
+            return (
+                <div><h1>This is ASP.Net</h1></div>
+            )
+        } else if(this.state.activeTab === 5) {
+            return (
+                <div><h1>This is Android</h1></div>
+            )
+        } else if(this.state.activeTab === 6) {
+            return (
+                <div><h1>This is Unity</h1></div>
+            )
+        }
+    }
+
     render() {
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Java</Tab>
+                    <Tab>C#</Tab>
+                    <Tab>Swift</Tab>
+                    <Tab>ASP.Net</Tab>
+                    <Tab>Android</Tab>
+                    <Tab>Unity3D</Tab>
+                </Tabs>
+
+                    <Grid>
+                        <Cell col={12}>
+                            <div className="content">{this.toogleCategories()}</div>
+                        </Cell>
+                    </Grid>
+            </div>
         )
     }
 }
